3c07b036071fbad4c97003b35d15d55fcfbd4fa3
Hakjong Son <hakjong.son@gmail.com>
2018-08-06 05:15:11 +0900

    analyzer

---
+++ analyzer/README.md
@@ -0,0 +1,4 @@
+
+put 'polarity.csv' here!
+
+Download it from http://word.snu.ac.kr/kosac/lexicon.php
---
+++ analyzer/main.py
@@ -0,0 +1,47 @@
+from konlpy.tag import Kkma
+import pymysql
+import csv
+
+kkma = Kkma()
+
+
+def gen_pol_dic():
+    t = {}
+    with open('./polarity.csv', 'r', encoding='utf-8') as f:
+        rdr = csv.reader(f)
+        for line in rdr:
+            word = line[0].split('/')[0].replace('*', '')
+            val = line[7]
+            if val == 'POS' or val == 'NEG':
+                if word not in t:
+                    t[word] = {'POS': 0, 'NEG': 0}
+                t[word][val] += 1
+    for word in t:
+        t[word] = max(t[word], key=t[word].get)
+    return t
+
+
+pol_dic = gen_pol_dic()
+
+
+def analyze(text):
+    p = kkma.pos(text)
+    pos = 0;
+    neg = 0;
+    for p_item in p:
+        word = p_item[0]
+        if word not in pol_dic:
+            continue
+        pol = pol_dic[word]
+        if pol == 'POS':
+            pos += 1
+        else:
+            neg += 1
+
+    print('POS: %d / NEG: %d' % (pos, neg))
+
+    if pos > neg:
+        return 'POS'
+    else:
+        return 'NEG'
+
